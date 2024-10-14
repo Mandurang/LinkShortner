13759c40f91b2e5e3ff21709a2ea84a9b6c8c1ae
Mandurang <56937341+Mandurang@users.noreply.github.com>
2024-10-15 00:44:03 +0100

    Add project files.

---
+++ LinkShortner/Models/ShortenUrlRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace LinkShortner.Models
+{
+    public class ShortenUrlRequest
+    {
+        public string Url { get; set; } = string.Empty;
+    }
+}
---
+++ LinkShortner/Services/UrlShorteningService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LinkShortner.Services
+{
+    public class UrlShorteningService
+    {
+        public const int NumberOfCharsInShortLink = 7;
+        private const string Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+        private readonly Random _random = new Random();
+        private readonly ApplicationDbContext _dbContext;
+
+        public async Task<string> GenerateUniqueCode()
+        {
+            var codeChars = new char[NumberOfCharsInShortLink];
+
+            while (true)
+            {
+                for (var i = 0; i < NumberOfCharsInShortLink; i++)
+                {
+                    var randomIndex = _random.Next(Alphabet.Length - 1);
+
+                    codeChars[i] = Alphabet[randomIndex];
+                }
+
+                var code = new string(codeChars);
+
+                if (!await _dbContext.ShortenedUrls.AnyAsync(s => s.Code == code))
+                {
+                    return code;
+                }
+            }
+        }
+    }
+}
